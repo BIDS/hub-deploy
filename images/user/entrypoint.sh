2108e0ee9716aa0bdcea5978630f73b8d40f23ee
Min RK <benjaminrk@gmail.com>
2026-05-15 20:09:06 -0700

    simplify entrypoint

--- images/user/entrypoint.sh
+++ images/user/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash -l
-export CONDA_ROOT=/srv/.pixi/envs/default
-export MAMBA_ROOT_PREFIX=$CONDA_ROOT
-source $CONDA_ROOT/etc/profile.d/mamba.sh
-mamba activate $CONDA_ROOT
-exec tini -- "$@"
+set -e
+mamba activate $MAMBA_ROOT_PREFIX
+exec "$@"
