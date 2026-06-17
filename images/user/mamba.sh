80fa000c54f34362bd3fcd75383ef9a15ee58999
Min RK <benjaminrk@gmail.com>
2026-06-17 15:57:33 -0700

    fix bash activation

--- images/user/mamba.sh
+++ images/user/mamba.sh
@@ -1,3 +1,3 @@
 # activate default env in login shells
-eval "$($MAMBA_ROOT_PREFIX/bin/mamba shell hook)"
+eval "$($MAMBA_ROOT_PREFIX/bin/mamba shell hook -s bash)"
 mamba activate $MAMBA_ROOT_PREFIX
