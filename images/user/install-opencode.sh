9686b1b171a2f2f4fa7f25daab8240f359d9ee57
Min RK <benjaminrk@gmail.com>
2026-07-08 13:44:25 -0700

    patchelf biorouterd

--- images/user/install-opencode.sh
+++ images/user/install-opencode.sh
@@ -10,5 +10,5 @@ BIOROUTER_VERSION=v1.87.1
 curl -s -L https://github.com/BaranziniLab/biorouter/releases/download/${BIOROUTER_VERSION}/biorouter-headless-${platform}.tar.gz | tar -xzv --strip 1 -C "$CONDA_PREFIX"
 test -f $CONDA_PREFIX/bin/biorouter
 # fix and verify rpath to lookup libxcb
-patchelf --set-rpath '$ORIGIN/../lib' $CONDA_PREFIX/bin/biorouter
+patchelf --set-rpath '$ORIGIN/../lib' $CONDA_PREFIX/bin/biorouter*
 biorouter --help
