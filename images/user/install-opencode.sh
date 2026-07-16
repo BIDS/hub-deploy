329f1d5206beb707d8e6b08c04b821a9fd10aaf8
Min RK <benjaminrk@gmail.com>
2026-07-16 13:08:34 -0700

    biorouter 1.88.1

--- images/user/install-opencode.sh
+++ images/user/install-opencode.sh
@@ -6,7 +6,7 @@ platform="linux-x64"
 curl -s -L https://github.com/anomalyco/opencode/releases/download/${OPENCODE_VERSION}/opencode-${platform}.tar.gz | tar -xzv -C "$CONDA_PREFIX/bin"
 test -f $CONDA_PREFIX/bin/opencode
 
-BIOROUTER_VERSION=v1.87.1
+BIOROUTER_VERSION=v1.88.1
 curl -s -L https://github.com/BaranziniLab/biorouter/releases/download/${BIOROUTER_VERSION}/biorouter-headless-${platform}.tar.gz | tar -xzv --strip 1 -C "$CONDA_PREFIX"
 test -f $CONDA_PREFIX/bin/biorouter
 # fix and verify rpath to lookup libxcb
